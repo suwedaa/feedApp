a3d2bcc0cb16744f0d120b07be09a0ec060db47f
suwedaa <suweda.afrah@gmail.com>
2024-11-06 15:41:45 -0500

    changes

---
+++ src/main/java/com/bptn/feedApp/repository/FeedRepository.java
@@ -0,0 +1,16 @@
+package com.bptn.feedApp.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.bptn.feedApp.jpa.Feed;
+import com.bptn.feedApp.jpa.User;
+
+public interface FeedRepository extends JpaRepository<Feed, Integer>, PagingAndSortingRepository<Feed, Integer> {
+
+	Page<Feed> findByUser(User user, Pageable pageable);
+
+	Page<Feed> findByUserNot(User user, Pageable pageable);
+}
