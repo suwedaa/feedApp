e5b70fef4e719fb37b9b5f61ec039a204db04a5c
suwedaa <suweda.afrah@gmail.com>
2024-11-07 15:03:31 -0500

    jwt service

---
+++ src/main/java/com/bptn/feedApp/security/JwtService.java
@@ -0,0 +1,43 @@
+package com.bptn.feedApp.security;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.bptn.feedApp.provider.ResourceProvider;
+
+@Component
+public class JwtService {
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	ResourceProvider provider;
+
+	public String generateJwtToken(String username, long expiration) {
+
+		return JWT.create().withIssuer(this.provider.getJwtIssuer()).withAudience(this.provider.getJwtAudience())
+				.withIssuedAt(new Date()).withSubject(username)
+				.withExpiresAt(new Date(System.currentTimeMillis() + expiration))
+				.sign(HMAC512(this.provider.getJwtSecret()));
+	}
+
+	public DecodedJWT verifyJwtToken(String token) {
+
+		return JWT.require(HMAC512(this.provider.getJwtSecret())).withIssuer(this.provider.getJwtIssuer()).build()
+				.verify(token);
+	}
+
+	public String getSubject(String token) {
+
+		return JWT.require(HMAC512(this.provider.getJwtSecret())).withIssuer(this.provider.getJwtIssuer()).build()
+				.verify(token).getSubject();
+	}
+
+}
