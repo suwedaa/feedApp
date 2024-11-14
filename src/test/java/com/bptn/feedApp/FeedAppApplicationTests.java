ffb28b20e516f00a22a0fce36642b63c7854b78f
suwedaa <suweda.afrah@gmail.com>
2024-11-14 14:20:23 -0500

    add demo test

--- src/test/java/com/bptn/feedApp/FeedAppApplicationTests.java
+++ src/test/java/com/bptn/feedApp/FeedAppApplicationTests.java
@@ -1,11 +1,19 @@
 package com.bptn.feedApp;
 
+import org.h2.tools.Server;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FeedAppApplicationTests {
+	@MockBean
+	Server server;
 
+	@DisplayName("Demo Test")
 	@Test
 	void contextLoads() {
 	}
